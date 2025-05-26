ec768818fd7b90d02116061bfccb78c0c4916916
MLiondis <michael.liondis@education.nsw.gov.au>
2025-05-26 11:56:59 +1000

    3 and 2

---
+++ project/py_scripts/2.digital_sensor copy.py
@@ -0,0 +1,26 @@
+from machine import Pin
+from time import sleep
+
+#idle to become ready 
+sleep(0.1)
+
+#store output pin in a variable
+led_pin = 25
+led2_pin = 20
+data_pin = 13
+
+#configure gpio pin as output and create object for pin class
+led = Pin(led_pin, Pin.OUT)
+led2 = Pin(led2_pin, Pin.OUT)
+
+#confugure gpio pin for input
+data = Pin(data_pin, Pin.IN)
+
+while True:
+    if data.value() == 1:
+        led.value(True)
+        led2.value(False)
+    else:
+        led.value(False)
+        led2.value(True)
+    sleep(0.1)
---
+++ project/py_scripts/3.analog_sensor.py
@@ -0,0 +1,31 @@
+from machine import Pin, ADC
+from time import sleep
+
+#idle to become ready 
+sleep(0.1)
+
+#store output pin in a variable
+led_pin = 25
+led2_pin = 20
+data_pin = 13
+analog_data_pin = 26
+
+#configure gpio pin as output and create object for pin class
+led = Pin(led_pin, Pin.OUT)
+led2 = Pin(led2_pin, Pin.OUT)
+
+#confugure gpio pin for input
+data = Pin(data_pin, Pin.IN)
+
+#configure gpio as adc pin
+adc_value = ADC(Pin(analog_data_pin))
+
+while True:
+    if data.value() == 1:
+        led.value(True)
+        led2.value(False)
+    else:
+        led.value(False)
+        led2.value(True)
+    print(print(f"Analog: {adc_value}"))
+    sleep(0.1)
